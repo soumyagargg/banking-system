259aaa78b7b590e67d257dde0967f2e11879a6f6
Soumay <soumayg995@gmail.com>
2025-07-13 13:13:32 +0530

    Structure and Function Declaration

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct Account {
+    char name[60];
+    int accNumber;
+    float balance;
+};
 
-int main() 
+// Functions declaration:
+void createAccount();
+void Deposit();
+void Withdraw();
+void Display();
+void FindAcc();
+
+
+int main()
 {
-    printf("Hello World\n");
+
 
     return 0;
 }
